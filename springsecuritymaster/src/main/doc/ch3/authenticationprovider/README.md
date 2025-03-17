5e784f1c252feace9378e55dfb707f40087c4dea
SmS <mongddangand@likelion.org>
2025-03-17 23:11:10 +0900

    [spring-security AuthenticationProvider]
    - AuthenticationProvider

--- springsecuritymaster/src/main/doc/ch3/authenticationprovider/README.md
+++ springsecuritymaster/src/main/doc/ch3/authenticationprovider/README.md
@@ -1,3 +1,18 @@
 ![img.png](img.png)
 ![img_1.png](img_1.png)
 ![img_2.png](img_2.png)
+![img_3.png](img_3.png)
+![img_4.png](img_4.png)
+![img_5.png](img_5.png)
+<br>
+코드로 보자.
+![img_6.png](img_6.png)
+<br>
+authentication 객체가 검증을 수행할 수 있는 적절한 객체인지를 supports에서 판단할 수 있다.
+<br>
+![img_7.png](img_7.png)
+<br>
+여러가지 종류의 AuthenticationProvider를 볼 수 있다. 그 중 <br>
+AnonymousAuthenticationProvider와 DaoAuthenticationProvider를 볼 수 있다. <br>
+
+생성과정은 생략 궁금할 시, 인증 제공자 AuthenticationProvider - 2 참조
--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,26 +1,25 @@
-//package io.security.springsecuritymaster;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
+package io.security.springsecuritymaster;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
 //        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
 //        AuthenticationManager authenticationManager = builder.build();
 ////        AuthenticationManager object = builder.getObject();
@@ -33,21 +32,26 @@
 //                .authenticationManager(authenticationManager)
 //                .addFilterBefore(customAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class);
 //        ;
-//
-//        return http.build();
-//    }
-//
+
+        http
+                .authorizeRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+
 //    public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
 //        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
 //        customAuthenticationFilter.setAuthenticationManager(authenticationManager);
 //
 //        return customAuthenticationFilter;
 //    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
-//}
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
