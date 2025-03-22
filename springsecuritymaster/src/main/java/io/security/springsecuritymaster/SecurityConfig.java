aeb502e41aeb5849c41caaab8cc4e37c1f5948d2
SmS <mongddangand@likelion.org>
2025-03-22 22:25:33 +0900

    [spring-security UserDetails]
    - UserDetails

--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -2,16 +2,11 @@ package io.security.springsecuritymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -35,15 +30,15 @@ public class SecurityConfig {
 
         http
                 .authorizeRequests(auth -> auth
-                        .requestMatchers("/").permitAll()
-                        .anyRequest().authenticated());
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService();
+         return new CustomUserDetailsService();
     }
 
 //    public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
