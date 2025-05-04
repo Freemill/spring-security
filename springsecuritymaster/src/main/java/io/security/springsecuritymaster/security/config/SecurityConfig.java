98357b24e0662921109c9124f27f26e4f700719f
SmS <mongddangand@likelion.org>
2025-05-05 00:13:21 +0900

    [practice prj - 6 . Logout ]

--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -1,18 +1,11 @@
 package io.security.springsecuritymaster.security.config;
 
-import io.security.springsecuritymaster.security.service.FormUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
